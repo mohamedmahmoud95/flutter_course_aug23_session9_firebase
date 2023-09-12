3f89c41e828d94283dcc9a4b5a24c064991d9abb
Mohamed Mahmoud <m.raslan97@gmail.com>
2023-09-12 21:47:35 +0300

    added user model

---
+++ lib/Models/user.dart
@@ -0,0 +1,17 @@
+class User{
+  String? name;
+  String? email;
+  String? password;
+  String? userId;
+  String? cartId;
+
+
+  User({
+    this.name,
+    this.email,
+    this.password,
+    this.userId,
+    this.cartId,
+});
+
+}
