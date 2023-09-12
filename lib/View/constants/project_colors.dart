e76694600a8aecde03874d33e7fb5902f6757d34
Mohamed Mahmoud <m.raslan97@gmail.com>
2023-09-12 21:37:28 +0300

    added project colors

---
+++ lib/View/constants/project_colors.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+/*
+Main 3 colors are: Blue, Green and turquoise
+ */
+Color mainBlue = const Color.fromARGB(255,17,114,188);
+
+Color mediumBlue = const Color.fromARGB(255,38,174,238);
+
+Color lightBlue = const Color.fromARGB(255,46,192,243);
+
+
+Color mainGreen = const Color.fromARGB(255,44,166,80);
+
+Color lightGreen = const Color.fromARGB(255,142,198,64);
+
+Color calmGreen = const Color.fromARGB(255,54,172,138);
+
+Color bluishGreen = const Color.fromARGB(255,46,168,202);
+
+Color bluishGrey = const Color.fromARGB(255,111,158,206);
+
+Color mint = const Color.fromARGB(255,197,222,224);
+
+
+
+
+
+
+
+
+
+
