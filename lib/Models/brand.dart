d7fba94af96f83c156af1f7d840569ef27847aae
Mohamed Mahmoud <m.raslan97@gmail.com>
2023-09-12 21:56:11 +0300

    added brand model

---
+++ lib/Models/brand.dart
@@ -0,0 +1,16 @@
+class Brand{
+  String? name;
+  String? id;
+  List<String>? productsIDs;
+  String? logoUrl;
+  double? rating;
+
+  Brand({
+    this.name,
+    this.id,
+    this.productsIDs,
+    this.logoUrl,
+    this.rating,
+});
+
+}
