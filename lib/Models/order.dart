617066b8d533fbb24f3713fa312bfc3e9d329d0b
Mohamed Mahmoud <m.raslan97@gmail.com>
2023-09-13 14:45:47 +0300

    build sign-in screen

---
+++ lib/Models/order.dart
@@ -0,0 +1,13 @@
+class Order{
+  String? id;
+  String? cartId;
+  String? userId;
+  String? shippingAddress;
+
+  Order ({
+    this.id,
+    this.cartId,
+    this.userId,
+    this.shippingAddress,
+});
+}
--- lib/View/Screens/landing_screen/landing_screen.dart
+++ lib/View/Screens/landing_screen/landing_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course_aug23_session9_firebase/View/constants/project_colors.dart';
 
 class LandingScreen extends StatefulWidget {
   const LandingScreen({Key? key}) : super(key: key);
@@ -8,10 +9,106 @@ class LandingScreen extends StatefulWidget {
 }
 
 class _LandingScreenState extends State<LandingScreen> {
+  bool passwordIsHidden = true;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
 
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+
+              const SizedBox(height: 100,),
+
+              Image.asset("assets/images/logo4.png"),
+
+
+
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+
+                  decoration: InputDecoration(
+
+                    labelText: "Email",
+
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: bluishGrey, width: 1),
+                    ),
+
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: mainBlue, width: 1),
+
+                    ),
+                  ),
+
+                ),
+              ),
+
+              const SizedBox(height: 20,),
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  obscureText: passwordIsHidden,
+                  decoration: InputDecoration(
+                    suffixIcon: InkWell(
+                      onTap: (){
+                        setState(() {
+                          passwordIsHidden = !passwordIsHidden;
+                        });
+                      },
+                      child:  Icon(
+                        passwordIsHidden ? Icons.visibility : Icons.visibility_off, // Use appropriate icons for visibility
+                      ),
+                    ),
+
+                    labelText: "Password",
+                      enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: bluishGrey, width: 1),
+                    ),
+
+                      focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: mainBlue, width: 1),
+
+                    ),
+                  ),
+
+                ),
+              ),
+
+              SizedBox(height: 20,),
+
+
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: mainBlue,
+                  minimumSize: const Size(200, 40),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                  onPressed: (){},
+                  child: const Text("Sign in"),
+              ),
+
+              
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
